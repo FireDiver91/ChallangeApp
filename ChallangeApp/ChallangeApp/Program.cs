f058d726a47e98401aa1a2eaacd84d7bc3f6eda6
FireDiver91 <michal.klimek2@gmail.com>
2023-01-21 14:14:49 +0100

    Zadanie dzień 4

--- ChallangeApp/ChallangeApp/Program.cs
+++ ChallangeApp/ChallangeApp/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// zmienna z imieniem
+// zmienna z płcią
+// zmienna z wiekiem
+// Kobieta poniżej 30 lat
+// Ewa, lat 33
+// Niepełnoletni mężczyzna
+
+var Imię = "Ewa";
+var Płeć = "Kobieta";
+var Wiek = 33;
+
+if (Imię != "Ewa")
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else if (Płeć != "Kobieta")
+{
+    Console.WriteLine("Niepełnoletni mężczyna");
+}
+else if (Wiek < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+
